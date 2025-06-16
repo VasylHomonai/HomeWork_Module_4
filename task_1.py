e31153e3c8c287a2a52d7eed8651729591e1fe72
Vasyl <vasja112@gmail.com>
2025-06-17 01:54:33 +0300

    modified task_1 & created .flake8

--- task_1.py
+++ task_1.py
@@ -2,19 +2,18 @@ from pathlib import Path
 
 path = Path('files/Salaries.txt')
 
+
 def total_salary(path) -> tuple:
     try:
         with open(path, encoding='utf-8') as file:
             list_numb = []  # Створюємо список для збереження в ньому всіх зарплат
             for line in file:
-                temp_list = line.strip().split(',') # Тимчасовий список для кожного рядку
-                list_numb.append(int(temp_list[1])) # Додаємо кожну зарплату до списку
-    
+                temp_list = line.strip().split(',')  # Тимчасовий список для кожного рядку
+                list_numb.append(int(temp_list[1]))  # Додаємо кожну зарплату до списку
         total = sum(list_numb)  # Вираховуємо загальну суму зарплат
-        average = round(total / len(list_numb)) # Вираховуємо середню суму зарплат
+        average = round(total / len(list_numb))  # Вираховуємо середню суму зарплат
 
-        return (total, average) # Передаємо результат
-    
+        return (total, average)  # Передаємо результат
     except FileNotFoundError:
         print(f"Файл {path} не знайдено.")
         return ()
@@ -24,11 +23,12 @@ def total_salary(path) -> tuple:
     except Exception as e:
         print(f"Сталася помилка: {e}")
         return ()
-    
+
+
 def print_result(total, average):
     print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
 
-    
+
 if __name__ == '__main__':
 
     result = total_salary(path)
